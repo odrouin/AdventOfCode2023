39476ce87ab70acfc7e00e855fadd48064de039f
Olivier Drouin <3201753+odrouin@users.noreply.github.com>
2023-12-03 14:09:01 -0500

    day 2 complete

--- AdventOfCode2023_2/Program.cs
+++ AdventOfCode2023_2/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+
+var part1Answer = 0;
+var part2Answer = 0;
+foreach (var line in lines)
+{
+    var pattern = @"^Game (\d+):(.*)";
+    var gameNumberMatch = Regex.Match(line, pattern);
+    var gameId = int.Parse(gameNumberMatch.Groups[1].Value);
+    var gameLine = gameNumberMatch.Groups[2].Value;
+    
+    var maxColors = new Dictionary<string, int>();
+    foreach (var pickLines in gameLine.TrimStart().Split("; "))
+    {
+        foreach (var colorLine in pickLines.Split(", "))
+        {
+            var splittedColorLine = colorLine.Split(" ");
+            var colorCount = int.Parse(splittedColorLine[0]);
+            var color = splittedColorLine[1];
+            if(!maxColors.ContainsKey(color) || colorCount > maxColors[color])
+            {
+                maxColors[color] = colorCount;
+            }
+        }
+    }
+    
+    // part 1
+    if(maxColors["red"] <= 12 && maxColors["green"] <= 13 && maxColors["blue"] <= 14)
+    {
+        part1Answer += gameId;
+    }
+    
+    // part 2
+    part2Answer += maxColors["red"] * maxColors["green"] * maxColors["blue"];
+}
+
+Console.WriteLine($"Part 1 Total: {part1Answer}");
+Console.WriteLine($"Part 2 Total: {part2Answer}");
