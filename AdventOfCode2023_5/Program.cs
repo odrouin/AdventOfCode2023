cfe86f3ac498b434ae3f3c8b1550e4e3ba992189
Olivier Drouin <3201753+odrouin@users.noreply.github.com>
2023-12-10 11:17:15 -0500

    test fonctionne, mais y'a un issue

--- AdventOfCode2023_5/Program.cs
+++ AdventOfCode2023_5/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System.Text.RegularExpressions;
 
-var lines = File.ReadAllLines("input.txt");
+var lines = File.ReadAllLines("test.txt");
 
-var seeds = GetNumbers(lines[0]);
-var seedTransformations = seeds.ToDictionary(x => x, x => x);
-var wasSeedTransformed = seeds.ToDictionary(x => x, x => false);
+var seeds = GetSeeds(lines[0]).ToList();
+var nextSeeds = new List<SeedRange>();
 
-foreach(var line in lines.Skip(1))
+foreach(var line in lines.Skip(2))
 {
     if (GetNumbers(line) is { Count: > 0 } numbers)
     {
@@ -14,33 +13,91 @@ foreach(var line in lines.Skip(1))
         var sourceRangeStart = numbers[1];
         var rangeLength = numbers[2];
 
-        foreach (var seed in seeds.Where(x => !wasSeedTransformed[x]))
+        var seedTransformationMin = sourceRangeStart;
+        var seedTransformationMax = seedTransformationMin + rangeLength - 1;
+        var seedTransformationDifference = destinationRangeStart - sourceRangeStart;
+        
+        foreach (var seed in seeds)
         {
-            var seedTransformation = seedTransformations[seed];
-            if (seedTransformation >= sourceRangeStart && seedTransformation < sourceRangeStart + rangeLength)
+            if (seed.Min >= seedTransformationMin && seed.Min <= seedTransformationMax && seed.Max > seedTransformationMax)
             {
-                var destination = destinationRangeStart + (seedTransformation - sourceRangeStart);
-                seedTransformations[seed] = destination;
-                wasSeedTransformed[seed] = true;
+                nextSeeds.Add(new SeedRange(seed.Min, Math.Min(seedTransformationMax, seed.Max), seedTransformationDifference));
+            }
+            else if (seed.Min < seedTransformationMin && seed.Max >= seedTransformationMin && seed.Max <= seedTransformationMax)
+            {
+                nextSeeds.Add(new SeedRange(Math.Max(seed.Min, seedTransformationMin), seed.Max, seedTransformationDifference));
+            }
+            else if(seed.Min > seedTransformationMin && seed.Max < seedTransformationMax)
+            {
+                nextSeeds.Add(new SeedRange(seed.Min, seed.Max, seedTransformationDifference));
+            }
+            else if (seed.Min < seedTransformationMin && seed.Max > seedTransformationMax)
+            {
+                nextSeeds.Add(new SeedRange(seedTransformationMin, seedTransformationMax, seedTransformationDifference));
             }
         }
     }
     else if (line.Length == 0)
     {
-        foreach (var seed in seeds)
+        // 1. Définir les nouveaux ranges 
+        var rangeEdges = 
+            nextSeeds.Concat(seeds)
+            .SelectMany(x => new[] { x.Min, x.Max, x.Min - 1, x.Max - 1 })
+            .Distinct()
+            .OrderBy(x => x).ToList();
+        
+        var newSeeds = new List<SeedRange>();
+        for (var i = 2; i < rangeEdges.Count; i+=2)
         {
-            wasSeedTransformed[seed] = false;
+            // ici ya de quoi de wrong
+            if (nextSeeds.FirstOrDefault(x => rangeEdges[i] >= x.Min && rangeEdges[i] <= x.Max) is { } nextSeed)
+            {
+                newSeeds.Add(new SeedRange(rangeEdges[i-1], rangeEdges[i], nextSeed.Difference));
+            }
+            else if(seeds.FirstOrDefault(x => rangeEdges[i] >= x.Min && rangeEdges[i] <= x.Max) is { })
+            {
+                newSeeds.Add(new SeedRange(rangeEdges[i-1], rangeEdges[i], 0));
+            }
         }
+        
+        // 2. Apply the differences
+        newSeeds = newSeeds.Select(x => new SeedRange(x.Min + x.Difference, x.Max + x.Difference, 0)).ToList();
+        
+        seeds = newSeeds;
+        nextSeeds = new List<SeedRange>();
     }
 }
 
-var seedLocations = seeds.Select(x => seedTransformations[x]).ToList();
+Console.WriteLine(seeds.Min(x => x.Min));
 
-Console.WriteLine(seedLocations.Min());
+IEnumerable<SeedRange> GetSeeds(string line)
+{
+    var numbers = GetNumbers(line);
+    for (var i = 0; i < numbers.Count; i += 2)
+    {
+        yield return new SeedRange(numbers[i], numbers[i] + numbers[i + 1] - 1, 0);
+    }
+}
 
 List<long> GetNumbers(string line)
 {
     return NumberRegex().Matches(line).Select(x => long.Parse(x.Value)).ToList();
+}
+
+class SeedRange
+{
+    public SeedRange(long min, long max, long difference)
+    {
+        this.Min = min;
+        this.Max = max;
+        this.Difference = difference;
+    }
+
+    public long Min { get; set; }
+
+    public long Max { get; set; }
+
+    public long Difference { get; set; }
 }
 
 partial class Program
