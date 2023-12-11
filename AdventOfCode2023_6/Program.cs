8519bb607397c46966c343910a8c7ca174570064
Olivier Drouin <3201753+odrouin@users.noreply.github.com>
2023-12-11 07:26:40 -0500

    combine part 1 & 2

--- AdventOfCode2023_6/Program.cs
+++ AdventOfCode2023_6/Program.cs
@@ -2,29 +2,73 @@
 
 var lines = File.ReadAllLines("input.txt");
 
-var time = long.Parse(NumberRegex().Match(lines[0].Replace(" ", "")).Value);
-var distanceToBeat = long.Parse(NumberRegex().Match(lines[1].Replace(" ", "")).Value);
+Console.WriteLine("Part 1");
+Console.WriteLine("================================");
+Console.WriteLine($"Total: {Part1()}");
+Console.WriteLine("\nPart 2");
+Console.WriteLine("================================");
+Console.WriteLine($"Total: {Part2()}");
 
-var numberOfWaysToWin = 0;
-for (var pressTime = 2L; pressTime < time; ++pressTime)
+long Part1()
 {
-    var releaseTime = time - pressTime;
-    var speed = pressTime;
-    var distance = speed * releaseTime;
-    if (distance > distanceToBeat)
-    {
-        numberOfWaysToWin++;
-    }
+    var times = NumberRegex().Matches(lines[0]).Select(x => int.Parse(x.Value)).ToArray();
+    var distances = NumberRegex().Matches(lines[1]).Select(x => int.Parse(x.Value)).ToArray();
 
-    // optimization
-    if (numberOfWaysToWin > 0 && distance < distanceToBeat)
+    var winPossibilities = new List<int>();
+    for (var raceNumber = 0 ; raceNumber < times.Length ; ++raceNumber)
     {
-        break;
-    }
-}
+        var numberOfWaysToWin = 0;
+        var time = times[raceNumber];
+        var distanceToBeat = distances[raceNumber];
+
+        for (var pressTime = 2; pressTime < time; ++pressTime)
+        {
+            var releaseTime = time - pressTime;
+            var speed = pressTime;
+            var distance = speed * releaseTime;
+            if (distance > distanceToBeat)
+            {
+                numberOfWaysToWin++;
+            }
+
+            // optimization
+            if (numberOfWaysToWin > 0 && distance < distanceToBeat)
+            {
+                break;
+            }
+        }
     
+        winPossibilities.Add(numberOfWaysToWin);
+    }
 
-Console.WriteLine(numberOfWaysToWin);
+    return winPossibilities.Aggregate((x, y) => x * y);
+}
+
+long Part2()
+{
+    var time = long.Parse(NumberRegex().Match(lines[0].Replace(" ", "")).Value);
+    var distanceToBeat = long.Parse(NumberRegex().Match(lines[1].Replace(" ", "")).Value);
+
+    var numberOfWaysToWin = 0;
+    for (var pressTime = 2L; pressTime < time; ++pressTime)
+    {
+        var releaseTime = time - pressTime;
+        var speed = pressTime;
+        var distance = speed * releaseTime;
+        if (distance > distanceToBeat)
+        {
+            numberOfWaysToWin++;
+        }
+
+        // optimization
+        if (numberOfWaysToWin > 0 && distance < distanceToBeat)
+        {
+            break;
+        }
+    }
+
+    return numberOfWaysToWin;
+}
 
 partial class Program
 {
