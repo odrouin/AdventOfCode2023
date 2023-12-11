e1f6b3400cd088041ceee3779a9450b4955df3a9
Olivier Drouin <3201753+odrouin@users.noreply.github.com>
2023-12-11 07:25:00 -0500

    part 2

--- AdventOfCode2023_6/Program.cs
+++ AdventOfCode2023_6/Program.cs
@@ -2,37 +2,29 @@
 
 var lines = File.ReadAllLines("input.txt");
 
-var times = NumberRegex().Matches(lines[0]).Select(x => int.Parse(x.Value)).ToArray();
-var distances = NumberRegex().Matches(lines[1]).Select(x => int.Parse(x.Value)).ToArray();
+var time = long.Parse(NumberRegex().Match(lines[0].Replace(" ", "")).Value);
+var distanceToBeat = long.Parse(NumberRegex().Match(lines[1].Replace(" ", "")).Value);
 
-var winPossibilities = new List<int>();
-for (var raceNumber = 0 ; raceNumber < times.Length ; ++raceNumber)
+var numberOfWaysToWin = 0;
+for (var pressTime = 2L; pressTime < time; ++pressTime)
 {
-    var numberOfWaysToWin = 0;
-    var time = times[raceNumber];
-    var distanceToBeat = distances[raceNumber];
-
-    for (var pressTime = 2; pressTime < time; ++pressTime)
+    var releaseTime = time - pressTime;
+    var speed = pressTime;
+    var distance = speed * releaseTime;
+    if (distance > distanceToBeat)
     {
-        var releaseTime = time - pressTime;
-        var speed = pressTime;
-        var distance = speed * releaseTime;
-        if (distance > distanceToBeat)
-        {
-            numberOfWaysToWin++;
-        }
-
-        // optimization
-        if (numberOfWaysToWin > 0 && distance < distanceToBeat)
-        {
-            break;
-        }
+        numberOfWaysToWin++;
     }
-    
-    winPossibilities.Add(numberOfWaysToWin);
-}
 
-Console.WriteLine(winPossibilities.Aggregate((x, y) => x * y));
+    // optimization
+    if (numberOfWaysToWin > 0 && distance < distanceToBeat)
+    {
+        break;
+    }
+}
+    
+
+Console.WriteLine(numberOfWaysToWin);
 
 partial class Program
 {
